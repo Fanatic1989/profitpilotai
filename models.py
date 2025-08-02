20ccff1b3c5817c5d4856673e03407941e9e1c19
Fanatic1989 <christiansucre1@gmail.com>
2025-08-02 10:43:59 -0400

    models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+USERS = {
+    "admin": {
+        "password": "8c6976e5b5410415bde908bd4dee15dfb16b94d6"  # 'admin' hashed using sha1
+    }
+}
+
+TRADE_LOGS = {
+    "admin": [
+        {"time": "2025-08-01 10:00", "pair": "EUR/USD", "result": "Win", "amount": "$100"},
+        {"time": "2025-08-01 10:05", "pair": "USD/JPY", "result": "Loss", "amount": "$50"}
+    ]
+}
+
+def init_db():
+    pass
