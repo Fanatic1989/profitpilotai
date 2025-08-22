a4205960491e711592aa4a7a1cc6d21d0360fe38
Fanatic1989 <christiansucre1@gmail.com>
2025-08-22 06:34:18 -0400

    Reset repo and add new profitpilot structure

--- auth.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-from models import USERS, TRADE_LOGS
-
-def verify_user(login_id, password):
-    user = USERS.get(login_id)
-    return user and user["password"] == hashlib.sha256(password.encode()).hexdigest()
-
-def get_user_dashboard(login_id):
-    return TRADE_LOGS.get(login_id, [])
